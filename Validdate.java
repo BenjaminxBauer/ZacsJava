16316c1f3e0d45d7881fc518781b3cd4871c7686
BenjaminxBauer <44845586+BenjaminxBauer@users.noreply.github.com>
2020-02-18 16:51:33 -0500

    pushed changes for validdate

--- Validdate.java
+++ Validdate.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. Testing if this shows up it updated.
  */
 package validdate; //package statement
 import java.util.Scanner; //import Scanner class
